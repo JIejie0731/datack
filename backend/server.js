0a77f4611c630f6c1a1d5c72d7284dfbb819465b
JIejie0731 <104480001+JIejie0731@users.noreply.github.com>
2025-05-21 17:03:50 +0800

    feat: 后端代码准备部署

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); // 新增
+const cors = require('cors');
 const dbStatus = require('./api/db-status');
+const fcExpress = require('@serverless-devs/express');
+
 const app = express();
-
-app.use(cors()); // 新增
-
+app.use(cors());
 app.get('/api/db-status', dbStatus);
 
-app.listen(3001, () => {
-  console.log('后端API本地服务已启动：http://localhost:3001');
-});
+// 适配为 FC handler
+module.exports.handler = fcExpress(app);
+
+// 本地开发时用 node server.js 也能跑
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(3001, () => {
+    console.log('后端API本地服务已启动：http://localhost:3001');
+  });
+}
